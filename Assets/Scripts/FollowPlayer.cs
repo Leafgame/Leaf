0bd8b9587ab553e592d5bee9418f681e983f255a
Edvin Klæbo <edvin.klaebo@gmail.com>
2016-10-24 15:20:38 +0200

    Basic Controller + Axis for ps3

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class FollowPlayer : MonoBehaviour
+	{
+
+		public Transform PlayersLocation;
+
+		public void Start ()
+		{
+			
+		}
+	
+		public void Update () {
+							
+		}
+	}
+}
---
+++ Assets/Scripts/InstantiatePlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class InstantiatePlayer : MonoBehaviour
+	{
+		public Vector3 StartLocation = new Vector3(0, 2, 0);
+
+		public GameObject Prefab;
+
+		public void Start ()
+		{
+			Instantiate( Prefab, StartLocation, Quaternion.identity );
+		}
+	}
+}
