d04e1257045506a2a72d9f2a4a0ab24b4e10350e
Edvin Klæbo <edvin.klaebo@gmail.com>
2016-11-06 10:13:43 +0100

    Improved character controller

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class CameraBehaviour : MonoBehaviour
+	{
+		[Header( "Camera" )]
+		public Camera MainCameraView;
+		public Rigidbody2D _rigidbody;
+		public float SmoothFollowSpeed = 10.0f;
+		public float CameraPanSpeed = 1.0f;
+
+		private bool _isGrounded;
+		private Vector3 _referenceVec;
+		private bool _cameraFollow;
+
+		public void Update( )
+		{
+			float verticalCamera = Input.GetAxis("VerticalCamera") * CameraPanSpeed;
+			float horizontalCamera = Input.GetAxis("HorizontalCamera") * CameraPanSpeed;
+
+			if (  Math.Abs(_rigidbody.velocity.x) > 0.1 || Math.Abs(_rigidbody.velocity.y) > 0.1 )
+				_cameraFollow = true;
+			else
+				_cameraFollow = false;
+
+			// Camera follow calculations
+			if (_cameraFollow)
+			{
+				_referenceVec = new Vector3(transform.position.x, transform.position.y, MainCameraView.transform.position.z);
+				MainCameraView.transform.position = Vector3.Lerp(MainCameraView.transform.position, _referenceVec,
+					SmoothFollowSpeed * Time.deltaTime);
+			}
+
+			MainCameraView.transform.Translate(horizontalCamera, verticalCamera, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/HoldButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldButton : MonoBehaviour
+{
+	public float JumpHoldTime = 0.5f;
+	void Update () {
+
+		if (Input.GetButton( "Fire1" ) && JumpHoldTime > 0)
+		{
+			Debug.Log( "Fire" );
+			JumpHoldTime -= Time.deltaTime;
+		}
+	}
+}
