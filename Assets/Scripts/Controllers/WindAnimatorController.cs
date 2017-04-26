a56c38fcbc251a58396350ffb1ee35092295f912
Edvin <edvin.klaebo@gmail.com>
2017-04-26 13:01:51 +0200

    Fixes & tweaks

---
+++ Assets/Scripts/Controllers/WindAnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.WindScripts;
+using UnityEngine;
+
+public class WindAnimatorController : MonoBehaviour
+{
+	public Animator Animator;
+	private WindObject _windObject;
+
+	private void Start()
+	{
+		_windObject = GetComponent<WindObject>();
+	}
+	private void Update ()
+	{
+		if (_windObject.IsActive)
+		{
+			Animator.speed = 1;
+		}
+		else
+		{
+			Animator.speed = 0;
+		}
+	}
+}
--- Assets/Scripts/WindScripts/MovableObject.cs
+++ Assets/Scripts/WindScripts/MovableObject.cs
@@ -5,12 +5,14 @@ namespace Assets.Scripts.Misc
 	public class MovableObject : MonoBehaviour
 	{
 		public float GrabRadius = 3f;
+		public bool IsMovable = true;
 
 		private Transform _playerReference;
         private Rigidbody2D _rigidbody2d;
-
+		private Vector3 _ogPosition;
 		public void Start()
 		{
+			_ogPosition = transform.position;
 			_rigidbody2d = GetComponentInParent<Rigidbody2D>();
 			if (_playerReference == null)
 			{
@@ -23,10 +25,14 @@ namespace Assets.Scripts.Misc
 			var diffVec = ( _playerReference.position - transform.position );
 			var distance = diffVec.magnitude;
 
-            if (Input.GetButton("Fire2") && distance < GrabRadius)
+            if (Input.GetButton("Fire2") && distance < GrabRadius && IsMovable)
 			{
 				var player = _playerReference.GetComponent<Player>();
                _rigidbody2d.velocity = new Vector2(player.velocity.x, 0);
+			}
+			if (!IsMovable)
+			{
+				transform.position = _ogPosition;
 			}
 		}
 
