8ad1b56c8fe01d78d1cca3f831e9f8b58689efce
Edvin Klæbo <edvin.klaebo@gmail.com>
2017-05-16 10:01:46 +0200

    Not really sure if this is worth commiting

---
+++ Assets/Scripts/Controllers/OnVisibleScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Controllers
+{
+	public class OnVisibleScript : MonoBehaviour
+	{
+		private List<GameObject> _gameObjects = new List<GameObject>();
+
+		private void Start()
+		{
+			var allObjects = FindObjectsOfType<GameObject>();
+			_gameObjects.AddRange(allObjects);
+		}
+
+		private void OnBecameVisible()
+		{
+			gameObject.SetActive(true);
+			print("Called visible");
+		}
+
+		private void OnBecameInvisible()
+		{
+			gameObject.SetActive(false);
+			print("Called invisible");
+		}
+		
+	}
+}
---
+++ Assets/Scripts/WindScripts/SlowFallZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Makes the player fall slowly down towards the ground
+/// </summary>
+public class SlowFallZone : MonoBehaviour
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="collision"></param>
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		
+	}
+
+}
