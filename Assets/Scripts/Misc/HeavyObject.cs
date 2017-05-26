9db5ecff8527d43bd76b9c5b992ba1976224ea0d
Edvin Klæbo <edvin.klaebo@gmail.com>
2017-05-26 18:44:05 +0200

    Restored deleted files

---
+++ Assets/Scripts/Misc/HeavyObject.cs
@@ -0,0 +1,50 @@
+﻿using Assets.Scripts.PlayerScripts;
+using UnityEngine;
+
+public class HeavyObject : MonoBehaviour
+{
+	/// <summary>
+	/// The initial position of the object when the game starts.
+	/// Use this to reset the postion of the object if it gets out of bounds
+	/// </summary>
+	public Vector3 InitialPosition;
+
+	[Range(0f, 1f)]
+	public float PushFactor = .25f;
+
+	private int pushMultiplier = 100;
+	private Rigidbody2D _rigidbody;
+
+	public void Start()
+	{
+		InitialPosition = transform.position;
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	public virtual BoxCollider2D WindBlockZone
+	{
+		get;
+		set;
+	}
+
+	public virtual void MoveObject()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	protected virtual void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.transform.tag == "Player")
+		{
+			var colrb = col.gameObject.GetComponent<Player>();
+			var playerwind = col.gameObject.GetComponentInChildren<PlayerItemsController>();
+			if (playerwind.InWindZone)
+			{
+				colrb.velocity = new Vector2(0, 1) * 30f;	
+			}
+			_rigidbody.velocity = new Vector2(colrb.velocity.x * PushFactor, 0);
+		}
+	}
+
+}
+
---
+++ Assets/Scripts/ParticleScript.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.WindScripts;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class ParticleScript : MonoBehaviour
+	{
+		private BoxCollider2D _windZoneBox;
+		public WindObject WindObjectRef;
+		private new ParticleSystem particleSystem;
+		private Transform _playerTransform;
+
+		public void Start()
+		{
+			particleSystem = GetComponent<ParticleSystem>();
+			particleSystem.Play();
+			_windZoneBox = WindObjectRef.GetComponent<BoxCollider2D>();
+			_playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		}
+
+		public void Update()
+		{
+			if (!WindObjectRef.IsActive)
+				particleSystem.enableEmission = false;
+			else
+				particleSystem.enableEmission = true;
+
+			if (tag != "Particles") return;
+
+			if(_windZoneBox.size.y > _windZoneBox.size.x)
+				particleSystem.startLifetime = ( _windZoneBox.size.y - 2.58f) / 6.5f; 
+			else
+				particleSystem.startLifetime = ( _windZoneBox.size.x - 2.58f) / 6.5f;
+
+		}
+
+	}
+}
